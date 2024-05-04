7274790b56bbedc34b91b7847199bf65fac69f8e
Souvik Biswas <souvik.biswas1998@gmail.com>
2024-05-04 16:13:43 +0530

    Dynamic height added in NgOptimizedImage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path : 'auth',
+        path: '',
         loadChildren: () => import('../app/authentication/auth.routes').then(c => c.authRoutes)
     }
 ];
--- src/app/authentication/auth/auth.component.ts
+++ src/app/authentication/auth/auth.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
+import { provideImgixLoader } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgOptimizedImage],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
+  providers: [provideImgixLoader("http://localhost:4200/assets/login-signup.webp"),]
 })
 export class AuthComponent {
-image="../../../assets/9610da61-a1a4-4215-b1fa-f612242d10e61698602536819-Flat_200--1-.avif";
-
 }
