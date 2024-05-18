67bdc59b037faa66b4b94d985f8599e32b05e482
Prem Ranjan Shaw <premranjanshaw@gmail.com>
2024-05-18 18:39:56 +0530

    added auth service and auth guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { CommonModule } from '@angular/common';
 import { FooterConfig } from './route.config';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, ToolbarModule, ButtonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [NgIf, RouterModule, ToolbarModule, ButtonModule, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) {
     router.events.subscribe(event => {
       // only interested in the NavigationEnd type of event
-      if (event instanceof NavigationEnd) this.showBottomNav = FooterConfig[event.url]    
+      if (event instanceof NavigationEnd) this.showBottomNav = FooterConfig[event.url]
     });
   }
 
